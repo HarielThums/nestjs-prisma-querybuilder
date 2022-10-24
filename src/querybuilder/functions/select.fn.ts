bb825413ce15e012d1358aa3a2bbd8416a99d446
Hariel <hariel@hotmail.com>
2022-10-24 09:47:04 -0300

    feat: select - filter and map empty spaces or empty properties

--- src/querybuilder/functions/select.fn.ts
+++ src/querybuilder/functions/select.fn.ts
@@ -2,9 +2,11 @@ export const select = (query) => {
   if (query.select) {
     const select = {};
 
-    query.select.split(' ').map((v: string) => {
-      select[v] = true;
-    });
+    query.select
+      .split(' ')
+      .map((v: string) => (v[0] === ' ' ? v.substring(1, v.length) : v))
+      .filter((v: string) => v !== '')
+      .map((v: string) => (select[v] = true));
 
     query.select = select;
   }
