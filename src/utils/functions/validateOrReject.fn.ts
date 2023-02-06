4ce301d8fdd0ff43dfe62347fc1f5accab3385dd
Hariel <hariel@hotmail.com>
2023-02-06 16:53:47 -0300

    feat: defaultValidateOrReject - recursive implementation

--- src/utils/functions/validateOrReject.fn.ts
+++ src/utils/functions/validateOrReject.fn.ts
@@ -1,28 +1,28 @@
 import { BadRequestException } from '@nestjs/common';
-import { validateOrReject } from 'class-validator';
+import { validateOrReject, ValidationError } from 'class-validator';
 
 export default async function defaultValidateOrReject(data) {
-  await validateOrReject(data).catch((errors) => {
-    const errMessages = [];
+  await validateOrReject(data).catch((errors: ValidationError[]) => {
+    const errMessages: string[] = [];
 
-    errors.forEach((err) => {
-      if (err.constraints) Object.values(err.constraints).forEach((val) => errMessages.push(val));
+    errors?.forEach((err: ValidationError) => {
+      if (err?.constraints) Object.values(err?.constraints).forEach((val) => errMessages.push(val));
 
       if (err?.children) {
-        let children = err.children;
-
-        do {
-          children.forEach((grandchild) => {
-            grandchild.children.forEach((greatGrandson) => {
-              Object.values(greatGrandson?.constraints).forEach((val) => errMessages.push(val));
-
-              children = grandchild.children;
-            });
-          });
-        } while (children.children);
+        err?.children?.forEach((children) => getErrMessages(errMessages, children));
       }
     });
 
     throw new BadRequestException({ statusCode: 400, message: errMessages });
   });
 }
+
+const getErrMessages = (errMessages: string[], children: ValidationError) => {
+  if (children?.constraints) {
+    Object?.values(children?.constraints)?.forEach((val) => errMessages.push(`${val}`));
+  }
+
+  if (children?.children) {
+    children?.children.forEach((v) => getErrMessages(errMessages, v));
+  }
+};
