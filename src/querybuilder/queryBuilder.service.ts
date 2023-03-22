46b0ce3b5b33b6a4dbe41c52746553191c929a56
Hariel <hariel@hotmail.com>
2023-03-22 15:00:03 -0300

    feat: QuerybuilderService - qs parse request query

--- src/querybuilder/queryBuilder.service.ts
+++ src/querybuilder/queryBuilder.service.ts
@@ -2,6 +2,7 @@ import { Global, Inject, Injectable } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { plainToClass } from 'class-transformer';
 import { Request } from 'express';
+import { inspect } from 'util';
 import { defaultPlainToClass } from '../utils/functions/plainToClass.fn';
 import defaultValidateOrReject from '../utils/functions/validateOrReject.fn';
 import { QueryResponse } from './dto/queryResponse.dto';
@@ -11,6 +12,7 @@ import { paginate } from './functions/paginate.fn';
 import { populate } from './functions/populate.fn';
 import { select } from './functions/select.fn';
 import { sort } from './functions/sort.fn';
+import * as qs from 'qs';
 
 @Global()
 @Injectable()
@@ -18,14 +20,15 @@ export class Querybuilder {
   constructor(@Inject(REQUEST) private readonly request: Request) {}
 
   /**
-   *
-   *
    * @param primaryKey PrimaryKey from model selected, default is '_id_'
+   * @param depth **QS depth definition**: The depth limit helps mitigate abuse when qs is used to parse user input, and it is recommended to keep it a reasonably small number. default is '_5_'
    * @returns {Promise<QueryResponse>} This will return your query to prisma
    * @seemore https://github.com/HarielThums/nestjs-prisma-querybuilder
    */
-  async query(primaryKey = 'id'): Promise<QueryResponse> {
-    const queryValidator = defaultPlainToClass(QueryValidator, this.request.query);
+  async query(primaryKey = 'id', depth = 5): Promise<QueryResponse> {
+    const requestQueryParsed = qs.parse(qs.stringify(this.request.query), { depth: depth });
+
+    const queryValidator = defaultPlainToClass(QueryValidator, requestQueryParsed);
 
     await defaultValidateOrReject(queryValidator);
 
