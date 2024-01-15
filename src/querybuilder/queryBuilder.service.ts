d6f07493444fe5c0ef89a7492385d534ce95f61b
Hariel <hariel@hotmail.com>
2024-01-15 15:27:46 -0300

    feat: Querybuilder - query setHeader param

--- src/querybuilder/queryBuilder.service.ts
+++ src/querybuilder/queryBuilder.service.ts
@@ -2,7 +2,7 @@ import { Global, Inject, Injectable } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { plainToClass } from 'class-transformer';
 import { Request } from 'express';
-import { inspect } from 'util';
+import * as qs from 'qs';
 import { defaultPlainToClass } from '../utils/functions/plainToClass.fn';
 import defaultValidateOrReject from '../utils/functions/validateOrReject.fn';
 import { QueryResponse } from './dto/queryResponse.dto';
@@ -12,7 +12,6 @@ import { paginate } from './functions/paginate.fn';
 import { populate } from './functions/populate.fn';
 import { select } from './functions/select.fn';
 import { sort } from './functions/sort.fn';
-import * as qs from 'qs';
 
 @Global()
 @Injectable()
@@ -22,26 +21,27 @@ export class Querybuilder {
   /**
    * @param primaryKey PrimaryKey from model selected, default is '_id_'
    * @param depth **QS depth definition**: The depth limit helps mitigate abuse when qs is used to parse user input, and it is recommended to keep it a reasonably small number. default is '_5_'
-   * @returns {Promise<QueryResponse>} This will return your query to prisma
+   * @param setHeaders define if will set response header 'page'
+   * @returns {Promise<Partial<QueryResponse>>} This will return your query to prisma
    * @seemore https://github.com/HarielThums/nestjs-prisma-querybuilder
    */
-  async query(primaryKey = 'id', depth = 5): Promise<QueryResponse> {
+  async query(primaryKey = 'id', depth = 5, setHeaders = true): Promise<Partial<QueryResponse>> {
     const requestQueryParsed = qs.parse(qs.stringify(this.request.query), { depth: depth });
 
     const queryValidator = defaultPlainToClass(QueryValidator, requestQueryParsed);
 
     await defaultValidateOrReject(queryValidator);
 
-    const query = this.buildQuery(queryValidator, primaryKey);
+    const query = this.buildQuery(queryValidator, primaryKey, setHeaders);
 
     return query;
   }
 
-  private buildQuery(query, primaryKey: string) {
+  private buildQuery(query, primaryKey: string, setHeaders: boolean) {
     query.page = Number(query.page) > 0 ? Number(query.page) : 1;
     query.limit = Number(query.limit) > 0 ? Number(query.limit) : 10;
 
-    this.request.res.setHeader('page', query.page);
+    if (setHeaders) this?.request?.res?.setHeader('page', query.page);
 
     query = sort(query);
 
